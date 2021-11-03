b81a6866da2a9429b632b4dce8948a3c6819e50e
GrantCheadle <>
2021-11-03 16:06:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner temp;
+    temp = new Scanner(System.in);
+    System.out.println("Enter the temperature in celcius: ");
+    double tempInput = temp.nextDouble();
+    double sum = (tempInput * 1.8) + 32;
+    System.out.print(sum);
+    
+  }
+}
