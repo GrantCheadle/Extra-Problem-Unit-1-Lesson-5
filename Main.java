17e1b87fe683bf85d86628c8a2d464a60223e115
Grant Cheadle <93667910+GrantCheadle@users.noreply.github.com>
2021-11-03 16:16:00 +0000

    Jacked up the code

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ class Main {
   public static void main(String[] args) {
     Scanner temp;
     temp = new Scanner(System.in);
-    System.out.println("Enter the temperature in celcius: ");
+    System.out.println("Enter the temperature in farenheit: ");
     double tempInput = temp.nextDouble();
-    double sum = (tempInput * 1.8) + 32;
+    double sum = (tempInput /1.8) - 32;
     System.out.print(sum);
 
     
